e5d01797c748ebfc3738530d67aa26f615e4162c
LucasNicolasIb <lucas.sm027@gmail.com>
2021-02-22 02:13:14 -0300

    Proyecto Servicio Tecnico

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ServicioTecnico.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
